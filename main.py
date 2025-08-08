0bfc4591e411fa8e48ab093ef824020f22b897ba
Daniel Nishimori <dankazukinishimori@gmail.com>
2025-08-08 15:14:16 -0300

    feat: add base functions / first challenge
    Was added in a primitive version the commands "depositar", "sacar",
    "extrato" and "sair", as purposed on the first challenge.
    
    PS, also testing a model of commits conventions.

--- main.py
+++ main.py
@@ -10,20 +10,40 @@ MENU = """
 AMOUNT_LIMIT = 500
 DRAW_LIMIT = 3
 balance = 0
-extract = ""
+statement = []
 draw_times = 0
 
 while True:
     command = input(MENU)
 
     if command == "d":
-        pass
+        value = float(input("Informe o valor do depósito: "))
+        if value <= 0:
+            print("valor insuficiente para ser depósitado.")
+            continue
+        balance += value
+        statement.append(f"Depósito: R$ {value:.2f}")
 
     elif command == "s":
-        pass
+        if draw_times >= DRAW_LIMIT:
+            print("Limite de saques diário exedido.")
+            continue
+
+        value = float(input("Informe o valor ser sacado: "))
+        if 0 > value > 500:
+            print("Valor inválido.")
+            continue
+        elif value <= balance:
+            draw_times += 1
+            balance -= value
+            statement.append(f"Saque: R$ {value:.2f}")
+        else:
+            print("Saldo insuficiente.")
+            continue
 
     elif command == "e":
-        pass
+        for s in statement:
+            print(s)
 
     elif command == "q":
         break
